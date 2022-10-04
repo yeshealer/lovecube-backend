e60a63dbfd6911d4770be0e86758a0388fdbe2d6
yeshealer <yeshealer@gmail.com>
2022-10-05 04:19:21 +0900

    update

--- routes/record.js
+++ routes/record.js
@@ -6,22 +6,20 @@ const ObjectId = require("mongodb").ObjectId;
 recordRoutes.route("/saveData").post(function (req, response) {
     let db_connect = dbo.getDb();
     let myobj = {
-        $set: {
-            toFirstName: req.query.toFirstName,
-            toLastName: req.query.toLastName,
-            toNickName: req.query.toNickName,
-            describe: req.query.describe,
-            finalCardImage: req.query.finalCardImage,
-            fromFirstName: req.query.fromFirstName,
-            fromLastName: req.query.fromLastName,
-            fromNickName: req.query.fromNickName,
-            inscription: req.query.inscription,
-            moreMessage: req.query.moreMessage,
-            msgFinalCard: req.query.msgFinalCard,
-            occasion: req.query.occasion,
-            topCardImage: req.query.topCardImage,
-            yourMail: req.query.yourMail,
-        },
+        toFirstName: req.query.toFirstName,
+        toLastName: req.query.toLastName,
+        toNickName: req.query.toNickName,
+        describe: req.query.describe,
+        finalCardImage: req.query.finalCardImage,
+        fromFirstName: req.query.fromFirstName,
+        fromLastName: req.query.fromLastName,
+        fromNickName: req.query.fromNickName,
+        inscription: req.query.inscription,
+        moreMessage: req.query.moreMessage,
+        msgFinalCard: req.query.msgFinalCard,
+        occasion: req.query.occasion,
+        topCardImage: req.query.topCardImage,
+        yourMail: req.query.yourMail,
     }
     db_connect.collection("collection").insertOne(myobj, function (err, res) {
         if (err) throw err;
