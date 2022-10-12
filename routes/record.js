89a0cec2c32512089f05102e60cb7dcef438c62c
yeshealer <yeshealer@gmail.com>
2022-10-12 22:42:47 +0900

    update

--- routes/record.js
+++ routes/record.js
@@ -10,7 +10,7 @@ recordRoutes.route("/saveData").post(function (req, response) {
         toLastName: req.query.toLastName,
         toNickName: req.query.toNickName,
         describe: req.query.describe,
-        finalCardImage: req.body.finalCardImage,
+        finalCardImage: req.data.finalCardImage,
         fromFirstName: req.query.fromFirstName,
         fromLastName: req.query.fromLastName,
         fromNickName: req.query.fromNickName,
