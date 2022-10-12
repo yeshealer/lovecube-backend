cebe40cb1f7ae269d55a0891b76fde236c069a0e
yeshealer <yeshealer@gmail.com>
2022-10-12 21:27:44 +0900

    update

--- routes/record.js
+++ routes/record.js
@@ -2,6 +2,9 @@ const express = require("express")
 const recordRoutes = express.Router();
 const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json({ limit: '50mb' }));
 
 recordRoutes.route("/saveData").post(function (req, response) {
     let db_connect = dbo.getDb();
@@ -10,7 +13,7 @@ recordRoutes.route("/saveData").post(function (req, response) {
         toLastName: req.query.toLastName,
         toNickName: req.query.toNickName,
         describe: req.query.describe,
-        finalCardImage: req.query.finalCardImage,
+        finalCardImage: req.body.finalCardImage,
         fromFirstName: req.query.fromFirstName,
         fromLastName: req.query.fromLastName,
         fromNickName: req.query.fromNickName,
